b218ee85c4260aa3b18ec5a628078fa5c4b33a3b
Tom Warner <twarner@sofi.org>
2021-03-17 14:44:56 -0600

    deps

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    `java-library`
+    kotlin("jvm") version "1.4.31"
+}
+
+group = "com.sofi"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val logbackVersion = "1.2.2"
+    val jwtVersion: String by project
+    api("com.fasterxml.jackson.core:jackson-databind:2.10.5")
+    api("io.jsonwebtoken:jjwt-api:$jwtVersion")
+    testImplementation("ch.qos.logback:logback-classic:$logbackVersion")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "renovate-test-2"
