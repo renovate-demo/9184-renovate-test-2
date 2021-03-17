b65f410ae0b18593cf1ee328b12e2d5f09658d6b
Tom Warner <twarner@sofi.org>
2021-03-17 15:04:41 -0600

    property

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,6 @@ repositories {
 }
 
 dependencies {
-    val logbackVersion = "1.2.2"
-    val jwtVersion: String by project
     api("com.fasterxml.jackson.core:jackson-databind:2.10.5")
-    api("io.jsonwebtoken:jjwt-api:$jwtVersion")
-    testImplementation("ch.qos.logback:logback-classic:$logbackVersion")
-    api("io.github.openfeign:feign-core:9.4.0")
+    api("io.jsonwebtoken:jjwt-api:${property("jwtVersion")}")
 }
